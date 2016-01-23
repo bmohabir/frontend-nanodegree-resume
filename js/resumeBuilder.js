1982a1bd302a20010c80396f69e49cd42bade928
Basil Mohabir <bmohabir01@gmail.com>
2016-01-22 21:12:28 -0500

    add bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name": "Basil Mohabir",
+	"role": "Front-End Padawan",
+	"contacts": {
+		"mobile": "917-375-2806",
+		"email": "bmohabir01@gmail.com",
+		"github": "github.com/bmohabir/",
+		"location": "Bronx, NY"
+	},
+	"biopic": "biopic.jpg",
+	"welcomeMessage": "Welcome to my web programming adventure!",
+	"skills": [
+		"HTML5", "JavaScript", "CSS3", "PC and Mac expert"
+	]
+	// TODO: add display function
+};
